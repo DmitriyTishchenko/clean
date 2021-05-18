a2f64bfbf644e1dfaf320bb1d08435d36ddcf438
DmitriyTishchenko <kt041084tdn@gmail.com>
2021-05-18 21:40:28 +0300

    Installing the Unyson framework

--- wp-content/themes/clean/index.php
+++ wp-content/themes/clean/index.php
@@ -1,41 +1,36 @@
 <?php
 get_header();
 ?>
-<?php if (is_front_page() && get_theme_mod('clean_home_category')): ?>
-    <div id="fh5co-portfolio">
-        <?php $query = new WP_Query(array(
-	        'category_name' => get_theme_mod('clean_home_category'),
-        )); ?>
 
-        <?php if ($query->have_posts()): $i=1; while ($query->have_posts()): $query->the_post(); ?>
+    <div id="fh5co-portfolio">
+		<?php if (have_posts()): $i=1; while (have_posts()): the_post(); ?>
             <!-- post -->
 
-            <?php if (has_post_thumbnail()){
-                $img_url = get_the_post_thumbnail_url();
-            }else{
-                $img_url = 'https://picsum.photos/1280/864';
-            }
-            ?>
+			<?php if (has_post_thumbnail()){
+				$img_url = get_the_post_thumbnail_url();
+			}else{
+				$img_url = 'https://picsum.photos/1280/864';
+			}
+			?>
 
             <div class="fh5co-portfolio-item <?php if ($i % 2 == 0)
-	            echo "fh5co-img-right"; ?>">
+				echo "fh5co-img-right"; ?>">
                 <div class="fh5co-portfolio-figure animate-box" style="background-image: url(<?php echo
-                $img_url; ?>);"></div>
+				$img_url; ?>);"></div>
                 <div class="fh5co-portfolio-description">
                     <h2><?php the_title(); ?></h2>
                     <p><?php the_content(''); ?></p>
                     <p><a href="<?php the_permalink(); ?>" class="btn btn-primary"><?php _e('Read more', 'clean');
-                    ?></a></p>
+							?></a></p>
                 </div>
             </div>
-        <?php $i++; endwhile; ?>
-        <?php else: ?>
+			<?php $i++; endwhile; ?>
+		<?php else: ?>
             <!-- no post found -->
 
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
+		<?php endif; ?>
 
     </div>
-<?php endif; ?>
+
 <?php
 get_footer();
